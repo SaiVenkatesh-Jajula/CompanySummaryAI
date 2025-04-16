8f674cc89be8f157d8abb0b7ba88734a42aa80c2
SaiVenkatesh-Jajula <109030073+SaiVenkatesh-Jajula@users.noreply.github.com>
2025-04-16 02:14:01 +0200

    Update CompanySummaryAI.py
    Added using Model llama3

--- CompanySummaryAI.py
+++ CompanySummaryAI.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+# from langchain_ollama import ChatOllama
 from WebScraping import get_company_info
 
 load_dotenv()
@@ -17,6 +18,7 @@ given the Title {title} and information {information} about a company, I want to
 
 summary_prompt_template = PromptTemplate(input_variables=["title","information"], template=summary_template)
 llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", openai_api_key=api_key)
+# llm=ChatOllama(model="llama3")
 
 chain= summary_prompt_template | llm
 res=chain.invoke(input={"title":title, "information":info})
